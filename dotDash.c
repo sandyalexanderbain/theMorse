83a85439ddc398aa291ee60fc3590d36e4380273
Alexander Bain <sandyalexanderbain@gmail.com>
2025-07-06 15:10:31 +0800

    modify dotDash.c working with all A-Z and 0-9 inputs with the switch cascade put into a function

--- dotDash.c
+++ dotDash.c
@@ -6,69 +6,40 @@
 #include <stdbool.h>
 
 #define DEBUG false
-#define check(X) printf(#X " = %d \n", X);
+#define check(X) if (DEBUG) printf(#X " = %d \n", X);
+
 #define five(Y, A, B, C, D, E) Y[0] = A; Y[1] = B, Y[2] = C; Y[3] = D; Y[4] = E;
 #define four(Y, A, B, C, D) Y[0] = A; Y[1] = B, Y[2] = C; Y[3] = D;
 #define three(Y, A, B, C) Y[0] = A; Y[1] = B, Y[2] = C;
+#define two(Y, A, B) Y[0] = A; Y[1] = B;
 
 void dot(void);
 void dash(void);
-int locate(char input);
+int locate(char userInput);
+void translate(char morsePrint[], int temporary);
 
 int main(void)
 {
-   char input, morsePrint[5] = {false};
-   int aP = 100; // artificialPointer variable
+   char userInput, morsePrint[5] = {false};
+   int temporary = 100; // artificialPointer variable
 
    printf("Enter a letter to convert to morse code: ");
-   scanf(" %c", &input);
+   scanf(" %c", &userInput);
    printf("\n     ");
 
-   if (DEBUG) check(aP);
-   aP = locate(input);
-   if (DEBUG) check(aP);
+   check(temporary);
+   temporary = locate(userInput);
+   check(temporary);
 
-   // Prevent Changing the first character to • when not valid input.
-   if (input != 'e' && aP == 0) aP = 100;
+   // Prevent Changing the first character to • when not valid userInput.
+   if (userInput != 'e' && temporary == 0) temporary = 100;
 
-   switch (aP) {
-      case 0: case 2: case 3: case 6: case 7: case 8: case 9: case 14: case 15: case 16: case 18:
-      case 20: case 21: case 30: case 31: case 33: case 37: case 40: case 45: {
-         morsePrint[0] = 1; 
-         break;
-      }
-   }
-
-   switch (aP) {
-      case 2: case 6: case 7: case 14: case 15: case 16: case 30: case 31: case 33: case 37:
-         morsePrint[1] = 1; break;
-   }
-
-   switch (aP) {
-      case 6: case 14: case 15: case 30: case 31: case 33:
-         morsePrint[2] = 1; break;
-   }
-
-   switch (aP) {
-      case 7: case 16: case 37:
-         morsePrint[2] = 2; break;
-   }
-
-   switch (aP) {
-      case 14: case 16: case 30: case 31: morsePrint[3] = 1; break;
-      case 15: case 33: case 37: morsePrint[3] = 2; break;
-   }
-
-   switch (aP) {
-      case 30: morsePrint[4] = 1; break;
-      case 31: case 33: case 37: morsePrint[4] = 2; break;
-   }
-
-   if (DEBUG) check(aP);
+   translate(morsePrint, temporary);
+   check(temporary);
 
    for (int lC = 0; lC < 5; lC++) { // loopCount variable
-      if (aP == 100) {
-         printf("%c\n\t is an invalid input!!! \n", input);
+      if (temporary == 100) {
+         printf("%c\n\t is an invalid userInput!!! \n", userInput);
          break;
       }
       if (morsePrint[lC] == 1) dot();
@@ -94,11 +65,11 @@ void dash() {
    printf("--- ");
 }
 
-int locate(char input)
+int locate(char userInput)
 {
    int value = 0;
 
-   switch (input) {
+   switch (userInput) {
       case 'a': value = 3; break;
       case 'b': value = 22; break;
       case 'c': value = 24; break;
@@ -108,7 +79,7 @@ int locate(char input)
       case 'g': value = 12; break;
       case 'h': value = 14; break;
       case 'i': value = 2; break;
-      case 'j': value = 20; break;
+      case 'j': value = 21; break;
       case 'k': value = 11; break;
       case 'l': value = 18; break;
       case 'm': value = 5; break;
@@ -121,7 +92,7 @@ int locate(char input)
       case 't': value = 1; break;
       case 'u': value = 7; break;
       case 'v': value = 15; break;
-      case 'w': value = 8; break;
+      case 'w': value = 9; break;
       case 'x': value = 23; break;
       case 'y': value = 25; break;
       case 'z': value = 26; break;
@@ -140,4 +111,54 @@ int locate(char input)
       case '/': value = 48; break;
    }
    return value;
+}
+
+void translate(char morsePrint[], int temporary)
+{
+   switch (temporary) {
+      case 30: five(morsePrint, 1, 1, 1, 1, 1); break;
+      case 31: five(morsePrint, 1, 1, 1, 1, 2); break;
+      case 33: five(morsePrint, 1, 1, 1, 2, 2); break;
+      case 37: five(morsePrint, 1, 1, 2, 2, 2); break;
+      case 40: five(morsePrint, 1, 2, 1, 2, 1); break;
+      case 45: five(morsePrint, 1, 2, 2, 2, 2); break;
+      case 46: five(morsePrint, 2, 1, 1, 1, 1); break;
+      case 47: five(morsePrint, 2, 1, 1, 1, 2); break;
+      case 48: five(morsePrint, 2, 1, 1, 2, 1); break;
+      case 54: five(morsePrint, 2, 2, 1, 1, 1); break;
+      case 58: five(morsePrint, 2, 2, 2, 1, 1); break;
+      case 60: five(morsePrint, 2, 2, 2, 2, 1); break;
+      case 61: five(morsePrint, 2, 2, 2, 2, 2); break;
+
+      case 14: four(morsePrint, 1, 1, 1, 1); break;
+      case 15: four(morsePrint, 1, 1, 1, 2); break;
+      case 16: four(morsePrint, 1, 1, 2, 1); break;
+      case 18: four(morsePrint, 1, 2, 1, 1); break;
+      case 20: four(morsePrint, 1, 2, 2, 1); break;
+      case 21: four(morsePrint, 1, 2, 2, 2); break;
+      case 22: four(morsePrint, 2, 1, 1, 1); break;
+      case 23: four(morsePrint, 2, 1, 1, 2); break;
+      case 24: four(morsePrint, 2, 1, 2, 1); break;
+      case 25: four(morsePrint, 2, 1, 2, 2); break;
+      case 26: four(morsePrint, 2, 2, 1, 1); break;
+      case 27: four(morsePrint, 2, 2, 1, 2); break;
+
+      case 6: three(morsePrint, 1, 1, 1); break;
+      case 7: three(morsePrint, 1, 1, 2); break;
+      case 8: three(morsePrint, 1, 2, 1); break;
+      case 9: three(morsePrint, 1, 2, 2); break;
+      case 10: three(morsePrint, 2, 1, 1); break;
+      case 11: three(morsePrint, 2, 1, 2); break;
+      case 12: three(morsePrint, 2, 2, 1); break;
+      case 13: three(morsePrint, 2, 2, 2); break;
+
+      case 2: two(morsePrint, 1, 1); break;
+      case 3: two(morsePrint, 1, 2); break;
+      case 4: two(morsePrint, 2, 1); break;
+      case 5: two(morsePrint, 2, 2); break;
+
+      case 0: morsePrint[0] = 1; break;
+      case 1: morsePrint[0] = 2; break;
+   }
+   return;
 }
